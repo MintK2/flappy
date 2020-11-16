00f7fcbc572cab77e4b70acc3fb9ade4c791d3d6
Andrew Westgerdes <ajwestgerdes@gmail.com>
2020-11-15 21:01:09 -0600

    base for AI config

--- flappy.py
+++ flappy.py
@@ -220,3 +220,20 @@ def main():
 
 
 main()
+
+
+def run():
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                                neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+    pop = neat.Population(config)
+    pop.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    pop.add_reporter(stats)
+
+    winner = pop.run(main, 50)
+
+
+if __name__ == "__main__":
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config-feedforward.txt")
+    run(config_path)
